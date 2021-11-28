6f6012cb927409f38e2a480a72d951c59d7da2c8
IkigaiFTW <89772648+IkigaiFTW@users.noreply.github.com>
2021-11-28 16:24:36 +0200

    added register view and mobile navbar component

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -6,11 +6,19 @@ import { AppComponent } from './app.component';
 import { LoginComponent } from './login/login.component';
 import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
 import { MatRippleModule } from '@angular/material/core';
+import { RegisterComponent } from './register/register.component';
+import { HomeComponent } from './home/home.component';
+import { MobileNavbarComponent } from './home/mobile/mobile-navbar/mobile-navbar.component';
+import { UserLogoComponent } from './components/user-logo/user-logo.component';
 
 @NgModule({
   declarations: [
     AppComponent,
-    LoginComponent
+    LoginComponent,
+    RegisterComponent,
+    HomeComponent,
+    MobileNavbarComponent,
+    UserLogoComponent,
   ],
   imports: [
     BrowserModule,
